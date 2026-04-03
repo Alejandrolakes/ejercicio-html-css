173e43376a4f8dc75c6e45fd38ac6477e8c5db42
Alejandro Lagos <alejandro.lagosjara@gmail.com>
2026-04-02 22:47:49 -0300

    Agregue scroll suave con boton de subir

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+function irArriba() { /* aca cree la funcion de irArriba (camelCase) */
+    window.scrollTo({ 
+        top: 0, /* aca le decimos a que altura tiene que llegar. siendo 0 el inicio de la pagina*/
+        behavior: "smooth" /* Define como se tiene que mover*/
+    });
+}
